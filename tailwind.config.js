406ac22793f6799ca07d55c991484eb92ad8a5df
erfankhedmati15 <erfankhedmati15@gmail.com>
2024-06-25 17:07:24 +0000

    add color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/App.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark": "#212121",
+        primary: {
+          500: "#E3D3FF",
+          1000: "#C29DFF"
+        },
+        secondary: "#484848"
+      }
+    },
   },
   plugins: [],
 }
